d9a78165171b30d99f9c67dadb3b8c492223c382
Chris Murphy <cmurphy@micros.com>
2013-07-15 11:28:19 +0100

    vibrate

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 window.onload  = function() {
 
-	// document.addEventListener("deviceready", onDeviceReady, false);    
+	document.addEventListener("deviceready", onDeviceReady, false);    
 
     function vibrate(duration) {
         navigator.notification.vibrate(duration);
     }
 
 	function onDeviceReady() {
-
-    }
-	
 	document.getElementById("toggle-colour").onclick = function() {
 		vibrate(500);
 		document.getElementById("h1").classList.toggle("turnMeRed");
 	}
+    }
+	
+
 
 
 }

9eb22d883bcc308aac90a48c4f372b2a9b5fd690
Chris Murphy <cmurphy@micros.com>
2013-07-15 11:19:54 +0100

    vibrate

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-window.onload  = function() {
+// window.onload  = function() {
 
-	// document.addEventListener("deviceready", onDeviceReady, false);    
-
-	// function onDeviceReady() {
- //        // Empty
- //    }
+	document.addEventListener("deviceready", onDeviceReady, false);    
 
     function vibrate(duration) {
         navigator.notification.vibrate(duration);
     }
 
+	function onDeviceReady() {
+		document.getElementById("toggle-colour").onclick = function() {
+			vibrate(500 500 500);
+			document.getElementById("h1").classList.toggle("turnMeRed");
+		}
+    }
 
-	document.getElementById("toggle-colour").onclick = function() {
-		vibrate(500 500 500);
-		document.getElementById("h1").classList.toggle("turnMeRed");
-	}
-}
+
+
+// }

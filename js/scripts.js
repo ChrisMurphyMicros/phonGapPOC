4e99f424db0290e39eb3547791ba54bd785a4abd
Chris Murphy <cmurphy@micros.com>
2013-07-15 15:13:40 +0100

    maps

--- js/scripts.js
+++ js/scripts.js
@@ -25,35 +25,35 @@ function onDeviceReady() {
 		}
 	}
 
-	document.getElementById("mapButton").onclick = function(e) {
-		e.preventDefault();
+	// document.getElementById("mapButton").onclick = function(e) {
+	// 	e.preventDefault();
 		
-		removeClassByClassName(document.getElementsByClassName("tabs"), "active");
-		document.getElementById("map").classList.add("active");
-	}
+	// 	removeClassByClassName(document.getElementsByClassName("tabs"), "active");
+	// 	document.getElementById("map").classList.add("active");
+	// }
 
 
 
 
-	var onSuccess = function(position) {
-	    alert('Latitude: '          + position.coords.latitude          + '\n' +
-	          'Longitude: '         + position.coords.longitude         + '\n' +
-	          'Altitude: '          + position.coords.altitude          + '\n' +
-	          'Accuracy: '          + position.coords.accuracy          + '\n' +
-	          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-	          'Heading: '           + position.coords.heading           + '\n' +
-	          'Speed: '             + position.coords.speed             + '\n' +
-	          'Timestamp: '         + new Date(position.timestamp)      + '\n');
-	};
+	// var onSuccess = function(position) {
+	//     alert('Latitude: '          + position.coords.latitude          + '\n' +
+	//           'Longitude: '         + position.coords.longitude         + '\n' +
+	//           'Altitude: '          + position.coords.altitude          + '\n' +
+	//           'Accuracy: '          + position.coords.accuracy          + '\n' +
+	//           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+	//           'Heading: '           + position.coords.heading           + '\n' +
+	//           'Speed: '             + position.coords.speed             + '\n' +
+	//           'Timestamp: '         + new Date(position.timestamp)      + '\n');
+	// };
 
-	// onError Callback receives a PositionError object
-	//
-	function onError(error) {
-	    alert('code: '    + error.code    + '\n' +
-	          'message: ' + error.message + '\n');
-	}
+	// // onError Callback receives a PositionError object
+	// //
+	// function onError(error) {
+	//     alert('code: '    + error.code    + '\n' +
+	//           'message: ' + error.message + '\n');
+	// }
 
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	// navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
 
 }

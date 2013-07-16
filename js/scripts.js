21fdff79cbd100f91a40c1765f00b3ae37808d8d
Chris Murphy <cmurphy@micros.com>
2013-07-16 13:45:36 +0100

    maps

--- js/scripts.js
+++ js/scripts.js
@@ -60,41 +60,47 @@ var infowindow;
 // function initialize() {
   // var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
 
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: currentLocation,
-    zoom: 15
-  });
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(geoLat, geoLong),
+		map: map
+	});
+	map = new google.maps.Map(document.getElementById('map-canvas'), {
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		center: currentLocation,
+		zoom: 16
+	});
 
-  var request = {
-    location: currentLocation,
-    radius: 1000,
-    query: "pub"
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-}
+	var request = {
+		location: currentLocation,
+		radius: 1000,
+		query: "pub"
+	};
+	infowindow = new google.maps.InfoWindow();
+	var service = new google.maps.places.PlacesService(map);
+		service.textSearch(request, callback);
+	}
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
-}
+	function callback(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0; i < results.length; i++) {
+				console.log("long: " + results[i].geometry.location.jb);
+				console.log("lat: " + results[i].geometry.location.kb);
+			  	createMarker(results[i]);
+			}
+		}
+	}
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
+	function createMarker(place) {
+		var placeLoc = place.geometry.location;
+		var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location
+	});
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent(place.name);
+		infowindow.open(map, this);
+	});
 // }
 
 
